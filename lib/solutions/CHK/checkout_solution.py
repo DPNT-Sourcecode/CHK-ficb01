2323600fe0042fb19a6815bf4bbe2e7a50b52153
sbx_user1051 <sbx_user1051@169.254.6.129>
2024-06-24 19:10:03 +0000

    Mon Jun 24 19:10:03 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,24 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+from dataclasses import dataclass
+
+
 price_table = {
     'A': 50,
     'B': 30,
     'C': 20,
     'D': 15
 }
+@dataclass
+class item:
+    sku: str
+    price: int
+    offer_quantity: int
+    offer_price: int
+offers = {
+
+}
 def checkout(skus):
     print(skus)
-
 
 
