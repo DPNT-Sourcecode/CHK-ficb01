459ed2ae01777ca72d644b7bf2ed886b010e32db
sbx_user1051 <sbx_user1051@169.254.6.129>
2024-06-24 19:32:41 +0000

    Mon Jun 24 19:32:41 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,5 +3,16 @@ from solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout(self):
         assert checkout_solution.checkout("A") == 50
+    def test_checkout_deal(self):
+        assert checkout_solution.checkout("AAA") == 130
+    def test_checkout_multideal(self):
+        assert checkout_solution.checkout("AAAAAA") == 260
+    def test_checkout_multideal_different(self):
+        assert checkout_solution.checkout("AAABB") == 175
+    def test_checkout_multideal_with_extra(self):
+        assert checkout_solution.checkout("AAABBD") == 190
+
+
+
 
 
